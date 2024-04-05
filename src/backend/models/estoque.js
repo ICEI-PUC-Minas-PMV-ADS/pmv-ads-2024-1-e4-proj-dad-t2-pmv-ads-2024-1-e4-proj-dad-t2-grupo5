a1b28467cd57cb183832fbe58612f8291c672911
Wellington Andrade Rolim <111004815+Wellingtonandrader@users.noreply.github.com>
2024-04-05 07:10:06 -0300

    Update estoque.js

--- src/backend/models/estoque.js
+++ src/backend/models/estoque.js
@@ -1,1 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
+// Definição do esquema Estoque.
+const estoqueSchema = new Schema({
+  nome: { type: String, required: true },
+  codigo: { type: String, required: true },
+  preco: { type: Number, required: true },
+  quantidade: { type: Number, required: true },
+});
+
+module.exports = mongoose.model('Estoque', estoqueSchema);
