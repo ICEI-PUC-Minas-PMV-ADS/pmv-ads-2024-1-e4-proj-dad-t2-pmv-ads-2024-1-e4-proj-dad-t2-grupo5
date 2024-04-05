1b9369da3d008a5ccee4f50727553f62db5799d1
Natan Fonseca <49421282+natann23@users.noreply.github.com>
2024-04-05 09:52:08 -0300

    Add files via upload

---
+++ src/backend/routes/atendimentoRoutes.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const router = express.Router();
+const Atendimento = require('../models/atendimento');
+
+// Listar todos os atendimentos
+router.get('/', async (req, res) => {
+  try {
+    const atendimentos = await Atendimento.find({})
+      .populate('medico', 'nome') // Popula o nome do médico
+      .populate('paciente', 'nome'); // Popula o nome do paciente
+    res.json(atendimentos);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Listar todas as atendimentos médicos de um paciente específico por ID
+router.get('/paciente/:pacienteId', async (req, res) => {
+    const pacienteId = req.params.pacienteId;
+    try {
+        const atendimentos = await Atendimento.find({ 'paciente': pacienteId });
+        res.json(atendimentos);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+// Listar atendimento específico por id
+router.get('/:atendimentoId', async (req, res) => {
+    const atendimentoId = req.params.atendimentoId;
+    try {
+        const atendimentos = await Atendimento.find({ '_id': atendimentoId });
+        res.json(atendimentos);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+// Criar um novo atendimento
+router.post('/', async (req, res) => {
+  try {
+    const { descricao, exameSolicitado, medico, paciente } = req.body;
+    const novoAtendimento = new Atendimento({
+      descricao,
+      exameSolicitado,
+      medico,
+      paciente
+    });
+    await novoAtendimento.save();
+    res.status(201).json({ message: 'Atendimento criado com sucesso!' });
+  } catch (error) {
+    res.status(400).json({ error: 'Falha ao criar atendimento', details: error });
+  }
+});
+
+module.exports = router;
