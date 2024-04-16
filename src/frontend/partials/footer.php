73c90bad5990c7304c4f44aa6cf3d04c8671cf5d
NeemiasLopes <90806252+NeemiasLopes@users.noreply.github.com>
2024-04-16 19:04:48 -0300

    Update footer.php
    Footer

--- src/frontend/partials/footer.php
+++ src/frontend/partials/footer.php
@@ -1,5 +1,23 @@
-<footer class="footer mt-auto py-3 bg-dark">
+<?php
+    $domain = $_SERVER['HTTP_HOST'];
+?>
+
+<style>
+    footer {
+        position: fixed;
+        bottom: 0;
+        width: 100%;
+        background-color: #333;
+        color: #fff;
+        text-align: center;
+        padding: 15px;
+    }
+</style>
+
+<footer class="footer py-3 bg-dark">
     <div class="container text-center">
-        <span class="text-light ">© 2024 VivaBem. Todos os direitos reservados.</span>
+        <span class="text-light"> R. Fulano de Tal da Silva, n° XXX - Bairro Aleatório, Cidade Desconhecida - UF, 00000-000
+        TEL: 31 1921-6161</span>
+        <p>&copy; 2024 Viva Bem. Todos os direitos reservados.</p>
     </div>
 </footer>
