cdbb53277547945d2c38bbba8c0f7fc5efb1e427
NeemiasLopes <90806252+NeemiasLopes@users.noreply.github.com>
2024-04-16 19:04:26 -0300

    Update header.php
    Header

--- src/frontend/partials/header.php
+++ src/frontend/partials/header.php
@@ -2,10 +2,22 @@
     $domain = $_SERVER['HTTP_HOST'];
 ?>
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<style>
+    .navbar-custom {
+        background-color: #AF1717; 
+    }
+    .navbar-custom .navbar-brand,
+    .navbar-custom .navbar-nav .nav-link {
+        color: white; 
+        font-size: 20px;
+        font-weight: bold;
+    }
+</style>
+
+<nav class="navbar navbar-expand-lg navbar-custom">
     <a class="navbar-brand" href="http://<?php echo $domain; ?>/prontuario">
-        <!-- <img src="seu-logotipo.png" width="30" height="30" class="d-inline-block align-top" alt="Logotipo"> -->
-        Logo
+        <img src="img/logo.png" width="30" height="30" class="d-inline-block align-top" alt="Logotipo">
+        Viva Bem
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -13,7 +25,7 @@
     <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav ml-auto">
             <li class="nav-item">
-                <a class="nav-link" href="http://<?php echo $domain; ?>/prontuario/frontend/fila/">fila</a>
+                <a class="nav-link" href="http://<?php echo $domain; ?>/prontuario/frontend/fila/">Fila</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="http://<?php echo $domain; ?>/prontuario/frontend/estoque/">Estoque</a>
