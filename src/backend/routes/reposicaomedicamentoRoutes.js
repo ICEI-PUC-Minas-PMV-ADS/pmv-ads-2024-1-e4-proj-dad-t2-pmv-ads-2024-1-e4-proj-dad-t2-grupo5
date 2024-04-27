d24046187b117eb33e995c124e7ebac21771789f
Wellington Andrade Rolim <111004815+Wellingtonandrader@users.noreply.github.com>
2024-04-27 12:14:10 -0300

    Update reposicaomedicamentoRoutes.js

--- src/backend/routes/reposicaomedicamentoRoutes.js
+++ src/backend/routes/reposicaomedicamentoRoutes.js
@@ -3,36 +3,44 @@ const router = express.Router();
 const Estoque = require('../models/estoque');
 const ReposicaoMedicamentos = require('../models/reposicaomedicamento');
 
-// Rota para reposição de medicamentos
 router.put('/repor-medicamentos/:id', async (req, res) => {
   try {
       const { id } = req.params;
 
-      // Verificar se a solicitação de reposição existe
       const reposicaoExistente = await ReposicaoMedicamentos.findById(id);
       if (!reposicaoExistente) {
           return res.status(404).json({ error: 'Solicitação de reposição não encontrada' });
       }
 
-      const medicamentosParaRepor = await Estoque.find({ quantidade: { $lt: 5 } });
+      // Limpar a tabela de medicamentos atuais
+      await ReposicaoMedicamentos.findByIdAndUpdate(id, { medicamentos: [] });
 
-      const medicamentosReposicao = medicamentosParaRepor.map(medicamento => ({
-          _id: medicamento._id, // Incluir o ID do medicamento
-          nome: medicamento.nome,
-          quantidadeFaltante: 5 - medicamento.quantidade 
+      // Encontrar todos os medicamentos com quantidade igual ou inferior a 5
+      const medicamentosAtuais = await Estoque.find({ quantidade: { $lte: 5 } });
+
+      // Mapear os medicamentos atuais para o formato desejado na tabela de reposição
+      const medicamentosReposicao = medicamentosAtuais.map(medicamento => ({
+        _id: medicamento._id, // Incluir o ID do medicamento
+        nome: medicamento.nome,
+        codigo: medicamento.codigo,
+        quantidadeAtual: medicamento.quantidade
       }));
 
-      // Atualizar a solicitação de reposição existente
+      // Atualizar a solicitação de reposição com os medicamentos atuais
       await ReposicaoMedicamentos.findByIdAndUpdate(id, { medicamentos: medicamentosReposicao });
 
       // Obter a solicitação de reposição atualizada
       const reposicaoAtualizada = await ReposicaoMedicamentos.findById(id);
 
+      // Registrar no console os medicamentos atualizados
+      console.log('Medicamentos atualizados:', medicamentosReposicao);
+
       res.status(200).json({ message: 'Solicitação de reposição atualizada com sucesso', reposicao: reposicaoAtualizada });
-  } catch (error) {
+    } 
+    catch (error) {
       console.error('Erro ao atualizar solicitação de reposição:', error);
       res.status(500).json({ error: 'Erro interno do servidor ao atualizar solicitação de reposição' });
-  }
+    }
 });
 
 // Get all medicamentos reposição
