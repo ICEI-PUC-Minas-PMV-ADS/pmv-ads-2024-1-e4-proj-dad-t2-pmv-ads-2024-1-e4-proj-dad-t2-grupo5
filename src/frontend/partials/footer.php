c7ef6263993ce8e842dcc8c6b4b204c016335e5e
ReisLucasF <reislucasf@icloud.com>
2024-05-02 22:27:26 -0300

    correção: shrink no footer para não sobrepor ao conteúdo

--- src/frontend/partials/footer.php
+++ src/frontend/partials/footer.php
@@ -1,15 +1,3 @@
-<style>
-    footer {
-        position: fixed;
-        bottom: 0;
-        width: 100%;
-        background-color: #333;
-        color: #fff;
-        text-align: center;
-        padding: 15px;
-    }
-</style>
-
 <footer class="footer py-3 bg-dark">
     <div class="container text-center">
         <span class="text-light"> R. Fulano de Tal da Silva, n° XXX - Bairro Aleatório, Cidade Desconhecida - UF, 00000-000
