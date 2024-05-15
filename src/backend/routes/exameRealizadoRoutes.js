3040996a2fe9bf6710e8870ebd23356ff9054d54
Wellingtonandrader <wellingtonandraderolim@hotmail.com>
2024-05-15 20:02:46 -0300

    trocamos a logica do GET

--- src/backend/routes/exameRealizadoRoutes.js
+++ src/backend/routes/exameRealizadoRoutes.js
@@ -2,31 +2,6 @@ const express = require('express');
 const router = express.Router();
 const ExameRealizado = require('../models/exameRealizado');
 
-// POST: Cadastrar um exame realizado
-// router.post('/', async (req, res) => {
-//     try {
-//         const { solicitacaoId, resultados, observacoes } = req.body;
-
-//         // Buscar a solicitação para copiar os exames e referências
-//         const solicitacao = await SolicitacaoExame.findById(solicitacaoId);
-//         if (!solicitacao) {
-//             return res.status(404).send({ message: 'Solicitação de exame não encontrada.' });
-//         }
-
-//         const novaRealizacao = new ExameRealizado({
-//             solicitacaoRef: solicitacaoId,
-//             resultados,
-//             dataRealizacao,
-//             observacoes
-//         });
-//         await novaRealizacao.save();
-//         res.status(201).send({ message: 'Exame realizado cadastrado com sucesso!' });
-//     } catch (error) {
-//         console.error(error);
-//         res.status(400).send({ error: 'Falha ao registrar o exame realizado', details: error.message });
-//     }
-// });
-
 // Criar uma nova receita médica
 router.post('/', async (req, res) => {
     try {
@@ -40,17 +15,16 @@ router.post('/', async (req, res) => {
 });
 
 
+
 // GET: Listar exames realizados por ID do paciente
 router.get('/realizados/paciente/:id', async (req, res) => {
     const pacienteId = req.params.id;
     try {
-        const examesRealizados = await ExameRealizado.find({ pacienteId: pacienteId })
-            .populate('medicoId', 'nome')
-            .populate('solicitacaoRef')
-            .exec();
-
+        // A consulta agora busca diretamente por 'solicitacaoRef.pacienteId'
+        const examesRealizados = await ExameRealizado.find({ "solicitacaoRef.pacienteId": pacienteId }).exec();
         res.json(examesRealizados);
     } catch (error) {
+        console.error("Erro ao buscar exames realizados:", error);
         res.status(500).json({ error: error.message });
     }
 });
