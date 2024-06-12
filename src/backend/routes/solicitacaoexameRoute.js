64dd56a633c178e40b823435a06f571f9c7e92ee
ReisLucasF <reislucasf@icloud.com>
2024-06-12 12:21:38 -0300

    enviado para produção

--- src/backend/routes/solicitacaoexameRoute.js
+++ src/backend/routes/solicitacaoexameRoute.js
@@ -17,58 +17,58 @@ router.get('/validar/:atendimentoId', async (req, res) => {
     }
 });
 
-// // Listar uma solicitação para um determinado ID de atendimento
-// router.get('/atendimento/:atendimentoId', async (req, res) => {
-//     const atendimentoId = req.params.atendimentoId;
-//     try {
-//         const solicitacao = await solicitacaoexame.findOne({ 'atendimentoRef.AtendimentoId': atendimentoId });
-//         if (solicitacao) {
-//             // Retornar a solicitação completa
-//             res.json(solicitacao);
-//         } else {
-//             res.status(404).json({ message: 'Solicitação de exame não encontrada.' });
-//         }
-//     } catch (error) {
-//         res.status(500).json({ error: 'Erro ao buscar a solicitação de exame', details: error });
-//     }
-// });
+// Listar uma solicitação para um determinado ID de atendimento
+router.get('/atendimento/:atendimentoId', async (req, res) => {
+    const atendimentoId = req.params.atendimentoId;
+    try {
+        const solicitacao = await solicitacaoexame.findOne({ 'atendimentoRef.AtendimentoId': atendimentoId });
+        if (solicitacao) {
+            // Retornar a solicitação completa
+            res.json(solicitacao);
+        } else {
+            res.status(404).json({ message: 'Solicitação de exame não encontrada.' });
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'Erro ao buscar a solicitação de exame', details: error });
+    }
+});
 
 
 
 
-// // Listar todos os exames que foram solicitados
-// router.get('/', async (req, res) => {
-//   try {
-//     const examesolicita = await solicitacaoexame.find({})
-//     res.json(examesolicita);
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// });
+// Listar todos os exames que foram solicitados
+router.get('/', async (req, res) => {
+  try {
+    const examesolicita = await solicitacaoexame.find({})
+    res.json(examesolicita);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
-// // Criar uma nova solicitação de exames
-// // POST
-// router.post('/', async (req, res) => {
-//     try {
-//         const novaSolicitacao= new solicitacaoexame (req.body);
-//         await novaSolicitacao.save();
-//         res.status(201).send({ message: 'Exame solucitado com sucesso!' });
-//     } catch (error) {
-//         res.status(400).send({ error: 'Falha ao solicitar exame', details: error });
-//     }
-// });
+// Criar uma nova solicitação de exames
+// POST
+router.post('/', async (req, res) => {
+    try {
+        const novaSolicitacao= new solicitacaoexame (req.body);
+        await novaSolicitacao.save();
+        res.status(201).send({ message: 'Exame solucitado com sucesso!' });
+    } catch (error) {
+        res.status(400).send({ error: 'Falha ao solicitar exame', details: error });
+    }
+});
 
-// // listar exames antigo do paciente
-// ///GET
+// listar exames antigo do paciente
+///GET
 
-// router.get('/paciente/:id', async (req, res) => {
-//     const pacienteId = req.params.id;
-//     try {
-//       const examesolicitado = await solicitacaoexame.find({ 'atendimentoRef.pacienteId': pacienteId });
-//       res.json(examesolicitado);
-//     } catch (error) {
-//       res.status(500).json({ error: error.message });
-//     }
-//   });
+router.get('/paciente/:id', async (req, res) => {
+    const pacienteId = req.params.id;
+    try {
+      const examesolicitado = await solicitacaoexame.find({ 'atendimentoRef.pacienteId': pacienteId });
+      res.json(examesolicitado);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  });
 
 module.exports = router;
